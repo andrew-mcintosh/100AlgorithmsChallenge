3ceb3683fd4e47870fa4ed4f7d4e33f2674c1fba
Andrew McIntosh <andrewmcintosh@Andrews-MacBook-Air-3.local>
2020-01-21 22:29:16 -0500

    completed addBorder challenge

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,16 @@
 function addBorder(picture: string[]): string[] {
+	const sizeOfWall = picture[0].length + 2;
+	let wall = '';
+	for (let i = 0; i < sizeOfWall; i++) {
+		wall = wall.concat('*');
+	}
+	picture.unshift(wall);
+	picture.push(wall);
 
+	for (let i = 1; i < picture.length - 1; i++) {
+		picture[i] = '*'.concat(picture[i]) + '*';
+	}
+	return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder([ 'abc', 'ded' ]));
